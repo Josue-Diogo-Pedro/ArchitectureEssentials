611c9321ffe0669648d8fe072598f667961cc77e
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-23 13:04:39 -0700

    OOP - add properties for Home. Obs: properties are in portuguese, i'll alter after to english

--- OOP/01 - Class x Object/Home.cs
+++ OOP/01 - Class x Object/Home.cs
@@ -3,5 +3,8 @@
 //Define class
 public class Home
 {
-
+    public int TamanhoM2 { get; set; }
+    public int Andares { get; set; }
+    public decimal Price { get; set; }
+    public int NumeroVagas { get; set; }
 }
