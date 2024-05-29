b00b9547b20dab04108da5ef44111d7a1a184e1c
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-29 10:15:20 -0700

    OOP - create class polymorphism and implement abstract methods from eletrodomestics

---
+++ OOP/02 - OOP Fundations/04 - Polymorphism.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP.Fundations;
+
+public class Polymorphism : Eletrodomestics
+{
+    public override void Off()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void On()
+    {
+        throw new NotImplementedException();
+    }
+}
