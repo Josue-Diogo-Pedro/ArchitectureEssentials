fbde84ea129fb85a148ec1ad169790b9d6c9e222
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-29 10:16:57 -0700

    OOP - generate constructors

--- OOP/02 - OOP Fundations/04 - Polymorphism.cs
+++ OOP/02 - OOP Fundations/04 - Polymorphism.cs
@@ -2,6 +2,12 @@
 
 public class Polymorphism : Eletrodomestics
 {
+    public Polymorphism(string name, int volt) : base(name, volt) { }
+
+    public Polymorphism() : base("Default", 220){ }
+
+
+
     public override void Off()
     {
         throw new NotImplementedException();
