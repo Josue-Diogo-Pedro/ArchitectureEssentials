8ba178d9ac0b4058faf5442ae297ccbc9c5a1271
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-30 10:00:49 -0700

    Modifiers, ProjectA - explanation of public and protected. Delete old class

---
+++ ProjectA/Classes.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjectA;
+
+public class Classes
+{
+
+}
+
+/***************************************************************************/
+// public:
+
+// Access is not restricted.
+/***************************************************************************/
+// protected:
+
+// Access is limited to the containing class or types
+// derived from containing class.
+/***************************************************************************/
