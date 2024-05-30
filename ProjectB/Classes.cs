48e085bb36181ac6711de1eadc19601bc754363e
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-05-30 10:39:22 -0700

    Modifiers, ProjectB - add test classes and modifiers of their class

---
+++ ProjectB/Classes.cs
@@ -0,0 +1,24 @@
+﻿using ProjectA;
+
+namespace ProjectB;
+
+class TestClasses
+{
+    public TestClasses()
+    {
+        Public publica = new();
+        //Private privada = new(); is not possible
+        //Internal interno = new(); is not possible
+        //Abstract abstracta = new(); is not possible
+    }
+}
+
+class TestModifier1
+{
+
+}
+
+class TestModifier2
+{
+
+}
