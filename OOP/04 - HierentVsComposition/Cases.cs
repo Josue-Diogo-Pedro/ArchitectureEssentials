b3441c16dcff12d21bd1d7a3416c0e8459bba1ac
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 14:37:25 -0700

    OOP - Hirient or Composition, case 1 about witch are of then is best to use!

---
+++ OOP/04 - HierentVsComposition/Cases.cs
@@ -0,0 +1,43 @@
+﻿namespace OOP.Fundations;
+
+#region Case1
+
+public class FisicPeople : People
+{
+    public string Cpf { get; set; }
+}
+
+public class FisicPeople2
+{
+    public People People { get; set; }
+    public string Cpf { get; set; }
+}
+
+public class TestHirientComposition
+{
+    public TestHirientComposition()
+    {
+        FisicPeople peopleHirient = new()
+        {
+            Name = "Josué",
+            BornsDate = DateTime.Now,
+            Cpf = "3214564334422"
+        };
+
+        FisicPeople2 peopleComposition = new()
+        {
+            People = new()
+            {
+                Name = "Josué",
+                BornsDate = DateTime.Now
+            },
+            Cpf = "09872525252525"
+        };
+
+        var nameHirient = peopleHirient.Name;
+        var nameComposition = peopleComposition.People.Name;
+    }
+}
+
+#endregion
+
