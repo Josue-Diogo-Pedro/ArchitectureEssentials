2bb64612ce32934d61eff09dc43e8edffc8c96d3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 16:14:38 -0700

    SOLID, SRP - create correct client class

---
+++ SOLID/01 - SRP/SRP.Soluction/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID.SRP.Soluction;
+
+public class Client
+{
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public Email Email { get; set; }
+    public Cpf Cpf { get; set; }
+    public DateTime InsertDate { get; set; }
+
+    public bool Validate() => Email.Validate() && Cpf.Validate();
+}
