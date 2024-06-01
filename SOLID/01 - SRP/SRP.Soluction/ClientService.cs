3c34f0dccd5c9d9122de4baf605905d6b6fa8eb3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 16:36:36 -0700

    SOLID, SRP - finish to create ClientService and explain about Single Responsability Principles

---
+++ SOLID/01 - SRP/SRP.Soluction/ClientService.cs
@@ -0,0 +1,16 @@
+﻿namespace SOLID.SRP.Soluction;
+
+public class ClientService
+{
+    public string AddClient(Client client)
+    {
+        if (!client.Validate()) return "Invlid datas";
+
+        var repo = new ClientRepository();
+        repo.AddClient(client);
+
+        EmailServices.Send("empresa@empresa.com", client.Email.Address, "Welcome!", "Congruatulations! You are inserted*(cadastrado, in portuguese rsrs)");
+
+        return "Client inserted with success";
+    }
+}
