b1fced1f580f8caaf4f24f02f2fec5ad7acd419c
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 12:59:34 -0700

    OOP - create class to explain concepts about interface and his implementation

---
+++ OOP/03 - InterfaceVsImplementation/Cases.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP.Fundations;
+
+public interface IRepository { }
+
+public class Repository { }
+
+public class RepositoryFake { }
+
+public class UseImplementation { }
+
+public class UseAbstraction { }
+
+public class TestInterfaceImplementation { }
