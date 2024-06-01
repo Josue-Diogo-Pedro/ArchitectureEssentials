f86096d370e6ed6be7a0e2a49166c8500be4e490
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 16:31:45 -0700

    SOLID, SRP - create EmailServices class

---
+++ SOLID/01 - SRP/SRP.Soluction/EmailServices.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Mail;
+
+namespace SOLID.SRP.Soluction;
+
+public static class EmailServices
+{
+    public static void Send(string from, string to, string subject, string message)
+    {
+        var mail = new MailMessage(from, to);
+        var client = new SmtpClient
+        {
+            Port = 25,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Host = "smtp.google.com"
+        };
+
+        mail.Subject = subject;
+        mail.Body = message;
+        client.Send(mail);
+        client.Dispose();
+    }
+}
