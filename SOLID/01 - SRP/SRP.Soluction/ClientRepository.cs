9646befc7fbb0c24ab18768f298e608abafc8ab6
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 16:23:10 -0700

    SOLID, SRP - finish to implement ClientRepository

--- SOLID/01 - SRP/SRP.Soluction/ClientRepository.cs
+++ SOLID/01 - SRP/SRP.Soluction/ClientRepository.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SOLID.SRP.Soluction;
 
-namespace SOLID._01___SRP.SRP.Soluction
+public class ClientRepository
 {
-    internal class ClientRepository
+    public void AddClient(Client client)
     {
+        using (var cn = new SqlConnection())
+        {
+            var cmd = new SqlCommand();
+
+            cn.ConnectionString = "MyConnectionString";
+            cmd.Connection = cn;
+            cmd.CommandType = CommnadType.Text;
+            cmd.CommandText = "INSERT INTO Client(Name, Email, Cpf, InsertDate) Values(@name, @email, @cpf, @insertdate)";
+
+            cmd.Parameters.AddWithValue("name", client.Name);
+            cmd.Parameters.AddWithValue("email", client.Email.Address);
+            cmd.Parameters.AddWithValue("cpf", client.Cpf.Number);
+            cmd.Parameters.AddWithValue("insertdate", client.InsertDate);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+        }
     }
 }
