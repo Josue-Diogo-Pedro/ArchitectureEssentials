75de1943ded42e34b594499e0c10226c432c3229
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 16:20:25 -0700

    SOLID, SRP - begin to implement ClientRepository in correct way to write code applying  SRP

---
+++ SOLID/01 - SRP/SRP.Soluction/ClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID._01___SRP.SRP.Soluction
+{
+    internal class ClientRepository
+    {
+    }
+}
