39877c6c9c802496f98430398fa8cd599b1b53e7
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 16:07:12 -0700

    SOLID, SRP - add client and send email finish, but in incorrect way

--- SOLID/01 - SRP/SRP.Violation/Client.cs
+++ SOLID/01 - SRP/SRP.Violation/Client.cs
@@ -1,4 +1,6 @@
-﻿namespace SOLID.SRP.Violation;
+﻿using System.Net.Mail;
+
+namespace SOLID.SRP.Violation;
 
 public class Client
 {
@@ -14,7 +16,37 @@ public class Client
 
         if (Cpf.Length != 11) return "Client with invalid cpf";
 
+        using (var cn = new SqlConnection())
+        {
+            var cmd = new SqlCommand();
 
-        return "";
+            cn.ConnectionString = "MyConnectionString";
+            cmd.Connection = cn;
+            cmd.CommandType = CommnadType.Text;
+            cmd.CommandText = "INSERT INTO Client(Name, Email, Cpf, InsertDate) Values(@name, @email, @cpf, @insertdate)";
+
+            cmd.Parameters.AddWithValue("name", Name);
+            cmd.Parameters.AddWithValue("email", Email);
+            cmd.Parameters.AddWithValue("cpf", Cpf);
+            cmd.Parameters.AddWithValue("insertdate", InsertDate);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        var mail = new MailMessage("empresa@empresa", Email);
+        var client = new SmtpClient
+        {
+            Port = 25,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Host = "smtp.google.com"
+        };
+
+        mail.Subject = "Welcome!";
+        mail.Body = "Congruatulations! You are inserted*(cadastrado, in portuguese rsrs)";
+        client.Send(mail);
+
+        return "Client inserted with success";
     }
 }
