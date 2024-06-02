462806c413af76881ba22540c6f58dcd9a86fe88
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 17:37:55 -0700

    OOP, OCP - create abstract class, or superclass to be a mother of implementations

---
+++ SOLID/02 - OCP/OCP.Soluction/CountDebit.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID.OCP.Soluction;
+
+public abstract class CountDebit
+{
+    public string TransactionNumber { get; set; }
+    public abstract string Debit(decimal value, string count);
+
+    public string TransactionFormater()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var random = new Random();
+        TransactionNumber = new string(Enumerable.Repeat(chars, 15).
+            Select(s => s[random.Next(s.Length)]).ToArray());
+
+        //number of formated transaction
+        return TransactionNumber;
+    }
+}
