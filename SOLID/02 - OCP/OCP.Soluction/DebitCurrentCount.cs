689dec396651e48646a8ca6d1e3d1a7eb051ab6a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 17:53:17 -0700

    SOLID, OCP - implement DebitCount in another class

---
+++ SOLID/02 - OCP/OCP.Soluction/DebitCurrentCount.cs
@@ -0,0 +1,10 @@
+﻿namespace SOLID.OCP.Soluction;
+
+public class DebitCurrentCount : CountDebit
+{
+    public override string Debit(decimal value, string count)
+    {
+        //Debit current count
+        return TransactionFormater();
+    }
+}
