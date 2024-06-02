dca2a8cc7d039657ae50bcba22bd8530d892275f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 18:21:42 -0700

    SOLID, OCP - create extension method to show another way of soluction

---
+++ SOLID/02 - OCP/OCP.Soluction.ExtensionMethods/DebitCurrentCount.cs
@@ -0,0 +1,10 @@
+﻿namespace SOLID.OCP.Soluction.ExtensionMethods;
+
+public static class DebitCurrentCount
+{
+    public static string DebitCurrentAccount(this CountDebit countDebit)
+    {
+        //business logic to debit current count
+        return countDebit.TransactionFormater();
+    }
+}
