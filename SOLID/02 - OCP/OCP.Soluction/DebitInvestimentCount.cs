3302d43bada0093470d7c7ccd471150277023589
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-01 18:15:19 -0700

    SOLID, OCP - add new class that implement mother class

---
+++ SOLID/02 - OCP/OCP.Soluction/DebitInvestimentCount.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLID.OCP.Soluction;
+
+public class DebitInvestimentCount : CountDebit
+{
+    public override string Debit(decimal value, string count)
+    {
+        //Debit investiment count
+        //Insert taxas
+        return TransactionFormater();
+    }
+}
