9d21938083855151f43ebbcc514bf1f4fee025fe
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-05 15:11:26 -0700

    SOLID, LSP - creating rectangular class and his atributtes

---
+++ SOLID/03 - LSP/LSP.Violacao/Rectangular.cs
@@ -0,0 +1,9 @@
+﻿namespace SOLID.LSP.Violacao;
+
+public class Rectangular
+{
+    public virtual double Height { get; set; }
+    public virtual double Width { get; set; }
+
+    public double Area { get => Height * Width; }
+}
