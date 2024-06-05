3712b7bcd1e3a36074b837118daeab6bf6d8044a
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-05 15:30:04 -0700

    SOLID, LSP - creating paralelogram class, correct form

---
+++ SOLID/03 - LSP/LSP.Solucao/Paralelogram.cs
@@ -0,0 +1,14 @@
+﻿namespace SOLID.LSP.Solucao;
+
+public abstract class Paralelogram
+{
+    public int Height { get; private set; }
+    public int Width { get; private set; }
+    public int Area { get => Height * Width; }
+
+    protected Paralelogram(int height, int width)
+    {
+        Height = height;
+        Width = width;
+    }
+}
