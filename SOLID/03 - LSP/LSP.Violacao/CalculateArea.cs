abe1ec0518488f85e9f73d95eab4265ee8b7c303
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-05 15:25:50 -0700

    SOLID, LSP - creating method to calculate area

---
+++ SOLID/03 - LSP/LSP.Violacao/CalculateArea.cs
@@ -0,0 +1,17 @@
+﻿namespace SOLID.LSP.Violacao;
+
+public class CalculateArea
+{
+    private static double ObtainRectangleArea(Rectangle rect) => rect.Area;
+
+    public static void Calculate()
+    {
+        var shape = new Shape
+        {
+            Height = 10,
+            Width = 5,
+        };
+
+        var area = ObtainRectangleArea(shape);
+    }
+}
