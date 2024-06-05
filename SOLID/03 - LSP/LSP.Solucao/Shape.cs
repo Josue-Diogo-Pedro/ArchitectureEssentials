7493b3e04011a26f1e4caf87151698577a3f4ee3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-05 15:34:41 -0700

    SOLID, LSP - create soluction of shape class

---
+++ SOLID/03 - LSP/LSP.Solucao/Shape.cs
@@ -0,0 +1,10 @@
+﻿namespace SOLID.LSP.Solucao;
+
+public class Shape : Paralelogram
+{
+    public Shape(int height, int width) : base(height, width)
+    {
+        if (height != width)
+            throw new ArgumentException("The two sides of shape need to be equals");
+    }
+}
