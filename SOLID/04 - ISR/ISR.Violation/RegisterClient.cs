514e220baf74cffb5e5a15812e276e8324425eef
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-06 13:30:21 -0700

    SOLID, ISP - class register client that implements icadastro

---
+++ SOLID/04 - ISR/ISR.Violation/RegisterClient.cs
@@ -0,0 +1,20 @@
+﻿namespace SOLID.ISR.Violation;
+
+public class RegisterClient : ICadastro
+{
+    public void ValidateDatas()
+    {
+        //validate CPF, Email
+    }
+
+    public void SaveDataBase()
+    {
+        //insert in client table    
+    }
+
+    public void SendEmail()
+    {
+        //send e-mail to client
+    }
+
+}
