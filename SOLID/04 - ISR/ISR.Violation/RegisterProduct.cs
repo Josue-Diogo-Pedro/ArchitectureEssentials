7450b333cc52afff95ffb4d63a7b455dbb80b2a9
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2024-06-06 13:35:25 -0700

    SOLID, ISP - create register product class and implement icadastro. So, this a wrong way to segragate concepts about register

---
+++ SOLID/04 - ISR/ISR.Violation/RegisterProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace SOLID.ISR.Violation;
+
+public class RegisterProduct : ICadastro
+{
+    public void ValidateDatas()
+    {
+        //validate values
+    }
+
+    public void SaveDataBase()
+    {
+        //insert in product table
+    }
+
+    public void SendEmail()
+    {
+        //Product don't have email, what can i do now??
+        throw new NotImplementedException("This method don't have implementation");
+    }
+
+}
